0a7dfda6e7a19d23aa5a1b7f95c3e2e4dbd44816
Ben Drucker <bvdrucker@gmail.com>
2015-03-30 10:44:40 -0400

    Initial implementation

---
+++ test/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import test from 'tape';
+import luhn from '../';
+
+
+test('Luhn', (t) => {
+
+  t.ok(luhn('4242424242424242'), 'passing');
+  t.notOk(luhn('4242424242424241'), 'failing');
+  t.end();
+
+});
