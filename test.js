864af869cddb4b9c44b8b009d5320e66725870a3
Kim Warzinski <Kim.Warzinski@bhnetwork.com>
2019-04-26 11:54:11 -0400

    fixing an issue where any number consisting of all zeros failed the luhn check

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ var luhn = require('./')
 
 test(function (t) {
   t.ok(luhn('4242424242424242'), 'passing')
+  t.ok(luhn('0000000000000000'), 'passing')
   t.notOk(luhn('4242424242424241'), 'failing')
   t.equal(luhn(''), false, 'falsy')
   t.end()
